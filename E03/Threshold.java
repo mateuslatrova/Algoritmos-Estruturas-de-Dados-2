d2470c7f7925903b059fdf1c39ef441b2c10388d
mateuslatrova <mateusls@ufmg.br>
2021-05-13 19:04:52 -0300

    added stdlib and tested Threshold

--- E03/Threshold.java
+++ E03/Threshold.java
@@ -13,16 +13,11 @@
   NUSP: 12542821
 
   Referências: 
-    - consultei a documentação das seguintes classes apresentadas em aula:
-        - StdOut: https://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/StdOut.html
-        - IndexMinPQ: https://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/IndexMinPQ.html
-        - Integer - https://docs.oracle.com/javase/7/docs/api/java/lang/Integer.html
-        - Character - https://docs.oracle.com/javase/7/docs/api/java/lang/Character.html
-    - recurso do enunciado: KnightOnePlain.java - o código foi forte-
-    mente baseado nessa implementação. Foram feitas apenas algumas mu-
-    danças(adição de novos atributos e métodos que ajudaram a resolver
-    o problema dada a otimização necessária).
-    - vídeo sobre o interval method: https://www.youtube.com/watch?v=OzFuihxtbtA
+    - API das classes Queue, StdDraw, UF, Point2D e Interval1D presen-
+    tes em: https://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/
+    - recurso do enunciado: GridDeluxe.java - o código foi fortemente
+    baseado nessa implementação.
+    - vídeo sobre o método de bissecção: https://www.youtube.com/watch?v=OzFuihxtbtA
     - página que visitei para aprender como arredondar números reais sem truncar:
     https://www.educative.io/edpresso/how-to-convert-a-double-to-int-in-java
 *********************************************************************/
@@ -37,28 +32,28 @@ import edu.princeton.cs.algs4.Interval1D;
 public class Threshold {
 
     // Constants:
-    private static final boolean numericalOnly = true;
-    private static final boolean graphicalAndNumerical = false;
+    private static final boolean numericalOnly = false;
+    private static final boolean graphicalAndNumerical = true;
     
     // Attribute:
-    private static boolean executionMode = numericalOnly;
+    private static boolean executionMode;
 
     private static class Vertex {
-        private static Point2D point;
-        private static int index;
+        public Point2D point;
+        public int index;
 
         private Vertex(Point2D p, int i) {
             point = p;
             index = i;
         }
 
-        public Point2D getPoint() {
-            return point;
-        }
-
-        public int getIndex() {
-            return index;
-        }
+        //public Point2D getPoint() {
+        //    return point;
+        //}
+//
+        //public int getIndex() {
+        //    return index;
+        //}
     }
 
     public static void drawGraph(Queue<Vertex> verts, Interval1D interval, double d) {
@@ -79,8 +74,8 @@ public class Threshold {
         }
 
 	    for (Vertex v : verts) {
-	        double x = v.getPoint().x();
-	        double y = v.getPoint().y();
+	        double x = v.point.x();
+	        double y = v.point.y();
 	        Point2D p  = new Point2D(x, y);
 
             int row = 1 + (int) (x * rows);
@@ -107,8 +102,8 @@ public class Threshold {
     } 
 
     public static void main(String[] args) {
-	    if (args.length > 1)
-            executionMode = graphicalAndNumerical;
+	    if (args.length <= 1)
+            executionMode = numericalOnly;
         
         Queue<Vertex> vertices = new Queue<Vertex>();
         Interval1D interval = new Interval1D(0, Math.sqrt(2));
@@ -139,14 +134,14 @@ public class Threshold {
             }
 
             for (Vertex v : vertices) {
-                int row = 1 + (int) (v.getPoint().x() * rows);
-                int col = 1 + (int) (v.getPoint().y() * cols);
+                int row = 1 + (int) (v.point.x() * rows);
+                int col = 1 + (int) (v.point.y() * cols);
                 for (int i = row-1; i <= row+1; i++) {
                     for (int j = col-1; j <= col+1; j++) {
                         for (Vertex u : grid[i][j]) {
-                            double currDist = v.getPoint().distanceTo(u.getPoint());
+                            double currDist = v.point.distanceTo(u.point);
                             if (currDist <= d) 
-                                graph.union(v.getIndex(), u.getIndex());
+                                graph.union(v.index, u.index);
                         }
                     }
                     grid[row][col].enqueue(v);
@@ -166,7 +161,54 @@ public class Threshold {
         StdOut.println(output);
         double d = (interval.max() + interval.min())/2;
         
-        if (executionMode == graphicalAndNumerical) 
-            drawGraph(vertices, interval, d);
+        if (executionMode == graphicalAndNumerical) {
+            int rows = (int) Math.ceil(1.0 / d);  
+            int cols = (int) Math.ceil(1.0 / d);
+        
+            StdDraw.setCanvasSize(800, 800);
+            StdDraw.setXscale(0, 1);
+            StdDraw.setYscale(0, 1);
+            StdDraw.enableDoubleBuffering();	
+	        
+            // for sanity check
+            StdDraw.setPenColor(StdDraw.RED);
+	        StdDraw.setPenRadius(0.02);
+	        (new Point2D(.0, .0)).drawTo(new Point2D(d, .0));
+	        StdDraw.setPenColor(StdDraw.BLACK);
+	        StdDraw.setPenRadius(0.005);
+
+            Queue<Point2D>[][] grid = (Queue<Point2D>[][]) new Queue[rows+2][cols+2];
+            for (int i = 0; i <= rows+1; i++) {
+                for (int j = 0; j <= cols+1; j++)
+                    grid[i][j] = new Queue<Point2D>();
+            }
+
+	        for (Vertex v : vertices) {
+	            double x = v.point.x();
+	            double y = v.point.y();
+	            Point2D p  = new Point2D(x, y);
+
+                int row = 1 + (int) (x * rows);
+                int col = 1 + (int) (y * cols);
+                for (int i = row-1; i <= row+1; i++) {
+                    for (int j = col-1; j <= col+1; j++) {
+                        for (Point2D q : grid[i][j]) {
+                            if (interval.contains(p.distanceTo(q))) {
+	        	                StdDraw.setPenRadius(0.02);
+                                StdDraw.setPenColor(StdDraw.RED);
+	        	                p.drawTo(q);
+	        	                StdDraw.setPenRadius(0.002);
+                                StdDraw.setPenColor(StdDraw.BLACK);
+                                continue;
+	        	            }
+                            p.drawTo(q);
+                        }
+                    }
+                    grid[row][col].enqueue(p);
+	                p.draw();
+                }
+            }  
+            StdDraw.show();
+        } 
     } 
 }
